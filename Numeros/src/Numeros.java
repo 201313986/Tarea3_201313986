c4fe3798d982a27fd65dcd32f82bd1f34acba047
201313986 Sergio Gerardo García Arteaga <sergio.arteag4@gmail.com>
2014-08-20 20:07:20 -0600

    Tarea3

--- Numeros/src/Numeros.java
+++ Numeros/src/Numeros.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 public class Numeros {
 
 	public static void main(String[] args) {
-		@SuppressWarnings("resource")
 		Scanner sc = new Scanner(System.in);
 		int numero;
 		System.out.println("ingrese un numero");
